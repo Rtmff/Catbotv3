92f8aae07cfaa9ac0a74d62bcd6ae4379535da31
Zeon <zeonchaster@gamil.com>
2022-03-13 09:21:24 +0700

    Catbotv2

---
+++ message/allmenu.js
@@ -0,0 +1,194 @@
+const allmenu = (prefix) => { 
+	return `*hallo kak ,* jangan spam ya kak, kasih jeda 5 detik agar bot tidak delay!, okeh kak?
+
+*⌜ Grup Menu ⌟* 
+• ${prefix}welcome <enable/disable>
+• ${prefix}antilink <enable/disable>
+• ${prefix}group <enable/disable>
+• ${prefix}simi <enable/disable>
+• ${prefix}kick <@user>
+• ${prefix}kickall
+• ${prefix}hedsot <@user>
+• ${prefix}reportbug
+• ${prefix}request
+• ${prefix}here
+• ${prefix}setdesk
+• ${prefix}setnamegc
+• ${prefix}setppgrup
+• ${prefix}promote
+• ${prefix}demote
+• ${prefix}wame
+• ${prefix}notif
+
+*⌜ Asupan Menu ⌟* 
+• ${prefix}asupanbunga
+• ${prefix}asupanayu
+• ${prefix}asupancaca
+• ${prefix}asupangeayubi
+• ${prefix}asupanaura
+
+*⌜ Maker Menu ⌟* 
+• ${prefix}blackpink <text>
+• ${prefix}pipe <text>
+• ${prefix}heloween <text>
+• ${prefix}heloween2 <text>
+• ${prefix}horor <text>
+• ${prefix}nulis <text>
+• ${prefix}sirkuit <text>
+• ${prefix}discovery <text>
+• ${prefix}fiction <text>
+• ${prefix}8bit <text>
+• ${prefix}demon <text>
+• ${prefix}transformer <text>
+• ${prefix}berry <text>
+• ${prefix}leyered <text>
+• ${prefix}thunder <text>
+• ${prefix}magma <text>
+• ${prefix}stone <text>
+• ${prefix}neon3 <text>
+• ${prefix}glitch <text>
+• ${prefix}glitch2 <text>
+• ${prefix}broken <text>
+• ${prefix}nulis2 <text>
+• ${prefix}gradient <text>
+• ${prefix}glossy <text>
+• ${prefix}watercolor <text>
+• ${prefix}multicolor <text>
+• ${prefix}neondevil <text>
+• ${prefix}underwater <text>
+• ${prefix}bear <text>
+
+*⌜ Game Menu ⌟* 
+• ${prefix}family100
+• ${prefix}tebakgambar
+• ${prefix}caklontong
+• ${prefix}tebakbendera
+
+*⌜ Anime Menu ⌟* 
+• ${prefix}ppcouple
+• ${prefix}uniform
+• ${prefix}cuckold
+• ${prefix}zettairyouiki
+• ${prefix}sfwneko
+• ${prefix}sao
+• ${prefix}cosplay
+• ${prefix}milf
+• ${prefix}loli
+• ${prefix}lovelive
+• ${prefix}hsdxd
+• ${prefix}husbu
+• ${prefix}wallml
+• ${prefix}waifu
+
+*⌜ Hewan Menu ⌟* 
+• ${prefix}fox
+• ${prefix}dog
+• ${prefix}cat
+• ${prefix}panda
+• ${prefix}panda1
+• ${prefix}bird
+• ${prefix}koala
+
+*⌜ Hentai Menu ⌟* 
+• Tobat Woy
+
+*⌜ Islam Menu ⌟* 
+• ${prefix}listsurah
+• ${prefix}asmaulhusna
+• ${prefix}alquran 
+• ${prefix}alquran 
+• ${prefix}alquran 
+• ${prefix}alquranaudio 
+• ${prefix}alquranaudio 
+• ${prefix}kisahnabi 
+• ${prefix}jadwalsholat 
+
+*⌜ Download Menu ⌟* 
+• ${prefix}play <judul lagu>
+• ${prefix}ytmp3 <judul/link>
+• ${prefix}ytmp4 <judul/link>
+• ${prefix}shortlink <link>
+• ${prefix}mediafire <link>
+• ${prefix}playstore <nama apk>
+• ${prefix}appsstore <nama apl>
+• ${prefix}y2mate <link>
+• ${prefix}tiktok <judul>
+• ${prefix}tiktokmusic <link>
+• ${prefix}tiktoknowm <link>
+• ${prefix}tiktokwm <link>
+• ${prefix}igvideo <link>
+• ${prefix}igfoto <link>
+• ${prefix}ktp <isi dengan lengkap>
+• ${prefix}pinterest <text>
+• ${prefix}ssweb <link>
+• ${prefix}lirik <text>
+• ${prefix}herolist <text>
+• ${prefix}herodetail <nama hero>
+
+*⌜ Serti Menu ⌟* 
+• ${prefix}serti1 <text>
+• ${prefix}serti2 <text>
+• ${prefix}serti3 <text>
+• ${prefix}serti4 <text>
+• ${prefix}serti5 <text>
+• ${prefix}serti6 <text>
+• ${prefix}serti7 <text>
+• ${prefix}serti8 <text>
+• ${prefix}serti9 <text>
+• ${prefix}serti10 <text>
+
+*⌜ Ocr Menu ⌟* 
+• ${prefix}namaninja
+• ${prefix}pantun
+• ${prefix}katasindiran
+• ${prefix}katailham
+• ${prefix}tongue
+• ${prefix}ssweb
+• ${prefix}nickepep
+
+*⌜ Sticker Menu ⌟* 
+• ${prefix}sticker <reply image>
+• ${prefix}ttp <text>
+• ${prefix}attp <text>
+• ${prefix}attp2 <text>
+• ${prefix}attp3 <text>
+• ${prefix}attp4 <text>
+• ${prefix}attp5 <text>
+• ${prefix}amongus <text>
+• ${prefix}patrick <text>
+• ${prefix}toimg <reply sticker>
+
+*⌜ Kode Menu ⌟*
+• ${prefix}tts <text>
+• ${prefix}kodenegara <text>
+• ${prefix}kodebahasa <text>
+
+*⌜ Owner Menu ⌟* 
+• ${prefix}broadcast <text>
+• ${prefix}broadcast2 <text>
+• ${prefix}broadcastvideo <text>
+• ${prefix}bcvideo <text>
+• ${prefix}bc <text>
+• ${prefix}bc2 <text>
+• ${prefix}bc3 <text>
+• ${prefix}bc4 <text>
+• ${prefix}bc5 <text>
+• ${prefix}delete <reply message>
+• ${prefix}ban <target>
+• ${prefix}unban <target>
+• ${prefix}block <user>
+• ${prefix}unblock <user>
+• ${prefix}settppbot <image>
+• ${prefix}leaveall (keluar semua grup)
+• ${prefix}join <link grup>
+• ${prefix}join2 <link grup>
+• ${prefix}clearall
+• ${prefix}restart
+• ${prefix}addcmd
+• ${prefix}delcmd
+• ${prefix}listcmd
+• ${prefix}exif
+`
+}
+
+exports.allmenu = allmenu
